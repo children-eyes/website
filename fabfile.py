1b618e44a096ac7a93d568944734acaddff23b7c
mono <monotom@gmail.com>
2014-01-19 23:34:55 +0100

    thining about how to use fabric

--- fabfile.py
+++ fabfile.py
@@ -1,92 +1,91 @@
 from __future__ import with_statement
 
 import fabric
-import os
 from fabric.api import *
 from fabric.colors import green, yellow, red
 from ftplib import FTP
 from git import *
+from fabric.operations import local as run_local
+import os as py_os
 
+run_ssh = run
+os_env  = py_os.environ
 
 config = {
+  'default': {
+    'git': {
+      'repo'   : 'git@github.com:children-eyes/website.git',
+      'user'   : '',
+      'pass'   : '',
+      'key'    : ''
+    },
+    'ftp': {
+      'host'   : '',
+      'user'   : '',
+      'pass'   : '',
+      'path' : ''
+    }
+  },
+
+  'local': {
+    'host'      : '127.0.0.1',
+  },
   'beta': {
     'hosts'      : ['beta.children-eyes.at'],
-    'git_repo'   : 'git@github.com:children-eyes/website.git',
-    'local_repo' : '/beta'
+    'path'       : '/beta',
+
   },
   'production': {
-   'hosts'      : ['children-eyes.at'],
-    'git_repo'   : 'git@github.com:children-eyes/website.git',
-    'local_repo' : '/production'
+    'hosts'      : ['children-eyes.at'],
+    'path'       : '/production'
   }
 }
+
+#TODO merge config default with concrete stages
 env.stages = config.keys()
 
 
+#stages
+#TODO generate stage methods 'local', 'beta', 'deploy' ans set env.stage if called
+  #def local(): _set_stage_config('local')
+#actions
+def deploy():
+  _enshure_stage_isset()
+  _check_not_stage('local')
 
-###############################################################################
-# SECTION: Constants
-###############################################################################
-DATABASE = {
-   "local": {
-      "userName": "user",
-      "password": "password"
-   }
-}
+  #TODO git checkout/update to local shared folder
+  #TODO sync with ftp
 
-FTP_ADDRESS = "ftp.something.com"
-FTP_USER = "user"
-FTP_PASSWORD = "password"
-FTP_ROOT_DIR = "/appDirectory"
-
-REPO_ROOT = "../"
-
-
-###############################################################################
-# SECTION: Private methods
-###############################################################################
-def _connectToFTP():
-   print green("** Connecting to the server **")
-
-   ftp = FTP(host=FTP_ADDRESS, user=FTP_USER, passwd=FTP_PASSWORD)
-   return ftp
-
-def _gitLatestFiles():
-   print green("** Connecting to Git **")
-
-   g = Git(REPO_ROOT)
-   repo = Repo(REPO_ROOT)
-   headCommit = repo.head.commit
-
-   print "Head commit revision: %s" % headCommit
-   print "Message: %s" % headCommit.message
-
-   result = g.execute(["git", "diff-tree", "--no-commit-id", "--name-only", "-r", str(headCommit)])
-   files = result.split("\n")
-
-   return _filterForValidFiles(fileList=files)
-
-def _filterForValidFiles(fileList):
-   return [f for f in fileList if f.startswith(("components/", "www/"))]
+def db(command):
+  if 'export' in command:
+    run('...')
+  elif 'import' in command:
+    run('...')
 
 
-###############################################################################
-# SECTION: Actions
-###############################################################################
+#helper
+def _enshure_stage_isset():
+  if not env.stage: raise "select stage first ("+_get_stages()+")"
 
-def uploadLatest():
-   print ""
-   print green("** Upload latest changes **")
+def _set_stage_config(stage):
+  os_env['ENVIRONMENT'] = stage #helps for testting
+  env.stage = stage
+  env.hosts = config[stage]['hosts']
+  env.config = config[stage]
+  _set_run_method()
 
-   ftp = _connectToFTP()
-   files = _gitLatestFiles()
 
-   for f in files:
-      print yellow("Uploading file %s" % f)
-      split = os.path.split(f)
+def _set_run_method():
+  global run
+  if env.hosts[0] is 'localhost' or env.hosts[0] is '127.0.0.1':
+    run = run_local
+  else:
+    run = run_ssh
 
-      ftp.cwd(os.path.join(FTP_ROOT_DIR, split[0]))
-      ftp.storlines("STOR %s" % split[1], open(os.path.join("../", f), "r"))
 
-   ftp.quit()
+def _check_not_stage(stage_name):
+  if env.stage is stage_name: raise Exception('you dont want to do this on stage "'+stage_name+'" ;)')
 
+
+def _check_stage(stage_name):
+  if not env.stage is stage_name: raise Exception('you only want to do this on stage "'+stage_name+'" ;)')
