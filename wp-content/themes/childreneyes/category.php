b82b7e1770a2300c823859258521a8bfa5c99ea8
mono <monotom@gmail.com>
2014-01-25 16:57:39 +0100

    remove category header

--- wp-content/themes/childreneyes/category.php
+++ wp-content/themes/childreneyes/category.php
@@ -17,7 +17,6 @@ get_header(); ?>
 			<?php if ( have_posts() ) : ?>
 
 			<header class="archive-header">
-				<h1 class="archive-title"><?php printf( __( 'Category Archives: %s', 'childreneyes' ), single_cat_title( '', false ) ); ?></h1>
 
 				<?php
 					// Show an optional term description.
